60b3a7332537043945a79fa32190b6d621ae114b
张文翔 <771797746@qq.com>
2020-01-30 00:02:48 +0800

    fix: 拼写错误

--- build/build.js
+++ build/build.js
@@ -6,7 +6,7 @@ const path = require('path')
 shell.echo('开始build')
 shell.rm('-rf', 'lib')
 shell.cp('-R', 'src/', 'lib')
-const pageageJson = JSON.parse(
+const packageJson = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, '../package.json')).toString()
 )
 // 删除package.json里多余的
@@ -19,13 +19,13 @@ const requireItem = [
   'license',
   'dependencies'
 ]
-for (const key in pageageJson) {
+for (const key in packageJson) {
   if (!requireItem.includes(key)) {
-    delete pageageJson[key]
+    delete packageJson[key]
   }
 }
 fs.writeFileSync(
   path.resolve(__dirname, '../lib/package.json'),
-  JSON.stringify(pageageJson, null, '\t')
+  JSON.stringify(packageJson, null, '\t')
 )
 shell.echo('build完毕，到lib下npm publish~')
