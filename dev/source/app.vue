46175b65c7167f15626cc003b8120a0566205cd0
张文翔 <771797746@qq.com>
2020-01-30 01:00:24 +0800

    fix: 删除多余代码和文件

--- dev/source/app.vue
+++ dev/source/app.vue
@@ -1,6 +1,4 @@
 <script>
-// 其实这儿应该是app.js?
-// 但是它又必须要过loader, 暂定为app.vue
 import indexPage from './pages/index/index.vue'
 import logsPage from './pages/logs/logs.vue'
 import { createApp } from '../../src'
